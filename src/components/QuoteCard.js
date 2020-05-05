3addd89b79efa165fa8ee4474e7ea6a22a70d896
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-05-04 18:18:29 -0700

    Done.

--- src/components/QuoteCard.js
+++ src/components/QuoteCard.js
@@ -14,7 +14,7 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-primary"
-          // onClick={upvoteQuote(props.quote.id)}
+            onClick={() => props.upvoteQuote(props.quote.id)}
           >
             Upvote
 
@@ -22,13 +22,14 @@ const QuoteCard = (props) =>
           <button
             type="button"
             className="btn btn-secondary"
-          // onClick={downvoteQuote(props.quote.id)}
+            onClick={() => props.downvoteQuote(props.quote.id)}
           >
             Downvote
           </button>
           <button
             type="button"
             className="btn btn-danger"
+            onClick={() => props.removeQuote(props.quote.id)}
           >
             <span aria-hidden="true">&times;</span>
           </button>
